8a50b354cf2e07efd0cb7293a155c02532e3d49b
arseny-tl <>
2019-08-19 03:17:16 +0300

    Add predicate test *Spring-chat helping
    Signed-off-by: arseny-tl <>

---
+++ src/test/java/com/tl/springexamples/functional/PredicateTest.java
@@ -0,0 +1,43 @@
+package com.tl.springexamples.functional;
+
+import com.tl.springexamples.model.Product;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+@Slf4j
+public class PredicateTest {
+
+  private static final Product zeroIdDateNow = new Product(0L, LocalDate.now());
+  private static final Product firstIdDateYesterday = new Product(1L, LocalDate.now().minusDays(1));
+  private final List<Product> products = new ArrayList<>();
+
+  @Before
+  public void setUp() {
+    products.add(zeroIdDateNow);
+    products.add(firstIdDateYesterday);
+  }
+
+  @Test
+  public void shouldReturnProductList_whereCreatedAtFilteredByPredicate() {
+    Predicate<Product> onlyYesterday = x -> x.getCreatedAt().isEqual(LocalDate.now().minusDays(1));
+    Predicate<Product> afterCurrentYear =
+      x -> x.getCreatedAt().getYear() == LocalDate.now().getYear();
+    List<Product> expectedYesterdayAndAfterCurrentYear = new ArrayList<>();
+    expectedYesterdayAndAfterCurrentYear.add(firstIdDateYesterday);
+
+    List<Product> actual =
+      products.stream()
+        .filter(onlyYesterday.and(afterCurrentYear))
+        .peek(p -> log.info(p.toString()))
+        .collect(Collectors.toList());
+
+    Assert.assertEquals(expectedYesterdayAndAfterCurrentYear, actual);
+  }
+}
---
+++ src/test/java/com/tl/springexamples/model/Product.java
@@ -0,0 +1,58 @@
+package com.tl.springexamples.model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Product {
+
+  private Long id;
+  private LocalDate createdAt;
+
+  public Product(Long id, LocalDate createdAt) {
+    this.id = id;
+    this.createdAt = createdAt;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Product)) {
+      return false;
+    }
+    Product product = (Product) o;
+    return Objects.equals(getId(), product.getId());
+  }
+
+  @Override
+  public int hashCode() {
+    int hash = 39;
+    hash += id == null ? 0 : Objects.hashCode(id);
+    return hash;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public LocalDate getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(LocalDate createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  @Override
+  public String toString() {
+    return "Product{" +
+      "id=" + id +
+      ", createdAt=" + createdAt +
+      '}';
+  }
+}
